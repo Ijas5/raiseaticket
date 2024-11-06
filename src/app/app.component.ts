079039f50caf2aca4f9305489c2751c2e6b2ca54
muhammed ijas <ijaskarassery45@gmail.com>
2024-11-06 11:52:58 +0530

    final1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit, OnDestroy, Renderer2} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component'; 
 import { CommonModule } from '@angular/common';
 import { PopupComponentComponent } from './popup-component/popup-component.component';
+import { Subscription } from 'rxjs';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +13,10 @@ import { PopupComponentComponent } from './popup-component/popup-component.compo
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  private authSubscription!: Subscription;
+
+  constructor(private authService: AuthService, private renderer: Renderer2) {}
   showProfilePopup = false;
 
   toggleProfilePopup(visible: boolean = !this.showProfilePopup) {
@@ -22,4 +27,24 @@ export class AppComponent {
     // Additional logout handling if needed
     this.showProfilePopup = false; // Close the popup on logout
   }
+  
+  ngOnInit() {
+    // Subscribe to authentication status and toggle the body's class
+    this.authSubscription = this.authService.isLoggedIn$.subscribe(isLoggedIn => {
+      if (isLoggedIn) {
+        this.renderer.addClass(document.body, 'logged-in');
+        this.renderer.removeClass(document.body, 'logged-out');
+      } else {
+        this.renderer.addClass(document.body, 'logged-out');
+        this.renderer.removeClass(document.body, 'logged-in');
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.authSubscription) {
+      this.authSubscription.unsubscribe();
+    }
+  }
+  
 }
--- src/app/changepassword/changepassword.component.ts
+++ src/app/changepassword/changepassword.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-changepassword',
   standalone: true,
@@ -9,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class ChangepasswordComponent {
 
-}
+    passwordVisible = false;
+  
+    constructor(private router: Router) {}
+  
+    togglePassword() {
+      this.passwordVisible = !this.passwordVisible;
+      const passwordInput = document.getElementById('password-input') as HTMLInputElement;
+      passwordInput.type = this.passwordVisible ? 'text' : 'password';
+    }
+  
+    goBack() {
+      this.router.navigate(['/dashboard']);
+    }
+  }
+  
+
